f7b4a39d06a55e1308902fb1a2ad8ab7cfbe692a
Rick Walker <rick.walker@agilent.com>
2026-08-06 18:29:25 -0700

    some tests of various pdplot commands

--- main.c
+++ main.c
@@ -101,255 +101,274 @@ int main(int argc, char *argv[])
     while(getz(s,BUF_SIZE) != EOF) {
        line++;
        sp=skipblanks(s);
-       if (isalpha(*sp)) {		// first non-white char is alpha, so a cmd
-	   if (strncmp(sp,"nextygraph",10)==0) {
-	       nextygraph();
-	   } else if (strncmp(sp,"replot",6)==0) {
-	       replot();
-	   } else if (strncmp(sp,"noframe",7)==0) {
-	       gridstate(0);
-	       box(0);
-	       scale(0);
-	       tick(0);
-   	   } else if (strncmp(sp,"framepen",8)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  frame_pen(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad framepen value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"gridpen",7)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  gridpen(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad grid value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"xgridpen",8)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  xgridpen(itmp); 
-	       } else {
-		  fprintf(stderr, "bad xgrid value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"ygridpen",8)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  ygridpen(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad ygrid value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"xgrid ",6)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  xgrid(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad xgrid value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"ygrid ",6)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  ygrid(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad ygrid value: %s\n", s);
-	       }
-   	   } else if (strncmp(sp,"grid ",5)==0) {
-       	       if (sscanf(s, "%*s %d", &itmp)==1) { 
-	       	  grid(itmp); 
-	       } else { 
-		  fprintf(stderr, "bad grid value: %s\n", s);
-	       }
-	   } else if (strncmp(sp,"brightgrid",10)==0) {
-	       gridpen(1);
-	   } else if (strncmp(sp,"dimgrid",7)==0) {
-	       gridpen(12);
-	   } else if (strncmp(sp,"linxy",4)==0) {
-	       logmode(0,0);
-	       logmode(0,0);
-	       logmode(1,0);
-	   } else if (strncmp(sp,"linx",4)==0) {
-	       logmode(0,0);
-	   } else if (strncmp(sp,"liny",4)==0) {
-	       logmode(1,0);
-	   } else if (strncmp(sp,"logxy",5)==0) {
-	       logmode(0,1);
-	       logmode(1,1);
-	   } else if (strncmp(sp,"logx",4)==0) {
-	       logmode(0,1);
-	   } else if (strncmp(sp,"logy",4)==0) {
-	       logmode(1,1);
-	   } else if (strncmp(sp,"loglog",6)==0) {
-	       logmode(0,1);
-	       logmode(1,1);
-	   } else if (strncmp(sp,"dbxy",4)==0) {
-	       logmode(0,2);
-	       logmode(1,2);
-	   } else if (strncmp(sp,"dbx",3)==0) {
-	       logmode(0,2);
-	   } else if (strncmp(sp,"dby",3)==0) {
-	       logmode(1,2);
-	   } else if (strncmp(sp,"dbpxy",5)==0) {
-	       logmode(0,3);
-	       logmode(1,3);
-	   } else if (strncmp(sp,"dbpx",4)==0) {
-	       logmode(0,3);
-	   } else if (strncmp(sp,"dbpy",4)==0) {
-	       logmode(1,3);
-	   } else if (strncmp(sp,"dump",4)==0) {
-	       dumppoints();
-	   } else if (strncmp(sp,"grid",4)==0) {
-	       gridstate(1);
-	   } else if (strncmp(sp,"nogrid",6)==0) {
-	       gridstate(0);
-	   } else if (strncmp(sp,"box",3)==0) {
-	       box(1);
-	   } else if (strncmp(sp,"nobox",5)==0) {
-	       box(0);
-	   } else if (strncmp(sp,"noisotropic",11)==0) {
-	       iso(0,1.0);
-	   } else if (strncmp(sp,"isotropic",9)==0) {
-	       if (sscanf(sp+9,"%lg", &tmp) != 1) {
-		   iso(1,1.0);
-	       } else {
-		   iso(1,tmp);
-	       }
-	   } else if (strncmp(sp,"plot",4)==0) {
-	       need_redraw++;
-	       xwin_top();
-	   } else if (strncmp(sp,"clear",5)==0) {
-	       initplot();	
-	   } else if (strncmp(sp,"xscaletol",9)==0) {
-	       if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
-	          fprintf(stderr, "bad xscaletol values: %s\n",sp);
-	       } else {
-		  xscaletol(tmp);	
-	       }
-	   } else if (strncmp(sp,"yscaletol",9)==0) {
-	       if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
-	          fprintf(stderr, "bad yscaletol values: %s\n",sp);
-	       } else {
-		  yscaletol(tmp);	
-	       }
-	   } else if (strncmp(sp,"scaletol",8)==0) {
-	       if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
-	          fprintf(stderr, "bad scaletol values: %s\n",sp);
-	       } else {
-		  scaletol(tmp);	
-	       }
-	   } else if (strncmp(sp,"ticklength",10)==0) {
-	       if (sscanf(sp+10,"%lg", &tmp) != 1 || tmp < 0.1 || tmp > 10.0) {
-		  tickset(-1.0);
-	       } else {
-		   tickset(tmp);	
-	       }
-	   } else if (strncmp(sp,"xset",4)==0) {
-	       if (sscanf(sp+4,"%lg %lg", &xmin, &xmax ) != 2 || xmin > xmax) {
-	          fprintf(stderr, "bad xset values: %s\n",sp);
-	       } else {
-	           // fprintf(stderr, "xset values: %g %g\n", xmin, xmax);
-		   xset(xmin, xmax);	
-	       }
-	   } else if (strncmp(sp,"yset",4)==0) {
-	       if (sscanf(sp+4,"%lg %lg", &ymin, &ymax ) != 2 || ymin > ymax) {
-	          fprintf(stderr, "bad yset values: %s\n",sp);
-	       } else {
-	           // fprintf(stderr, "yset values: %g %g\n", ymin, ymax);
-		   yset(ymin, ymax);	
-	       }
-	   } else if (strncmp(sp,"xscale",6)==0) {
-	       if (sscanf(sp,"xscale %lg %[^#]", &tmp, scratch ) == 2) {
-	       	  xscale(scratch,tmp);	
-	       } else if (sscanf(sp,"xscale %[^#]", scratch ) == 1) {
-	       	  xscale(scratch,1.0);	
-	       } else {
-		  fprintf(stderr, "bad xscale values: %s\n", sp);
-	       }
-	   } else if (strncmp(sp,"yscale",6)==0) {
-	       if (sscanf(sp,"yscale %lg %[^#]", &tmp, scratch ) == 2) {
-	       	  yscale(scratch,tmp);	
-	       } else if (sscanf(sp,"yscale %[^#]", scratch ) == 1) {
-	       	  yscale(scratch,1.0);	
-	       } else {
-		  fprintf(stderr, "bad yscale values: %s\n", sp);
-	       }
-	   } else if (strncmp(sp,"titlesize",9)==0) {	
-	       savecmd(sp); 	// prevent "title" from eating titlesize
-	   } else if (strncmp(sp,"title",5)==0) {
-	       title(sp+6);
-	   } else if (strncmp(sp,"style",5)==0) {
-       	       sp=skipblanks(sp+6);
-	       if (sp[0] == 'p') {
-	       	  gridstate(0); 
-		  setcharsize(1.6);
-	       } else if (sp[0] == 'w') {
-	          gridstate(1);
-		  setcharsize(1.0);
-	       }
-	   } else if (strncmp(sp,"graph",5)==0) {
-	       if (sscanf(sp,"graph %s", scratch) == 1) {
-		   sprintf(scratch2, "pnmtopng > %s.png", scratch);
-		   xwin_dump_graphics(scratch2);
-	       } else {
-		   xwin_dump_graphics("pnmtopng > pddump.png");
-	       }
-	   } else if (strncmp(sp,"autoplot",8)==0) {
-	       ps_set_outputtype(AUTOPLOT);
-	       if (sscanf(sp,"autoplot %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.ap", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.ap");
-	       }
-	   } else if (strncmp(sp,"pig",3)==0) {
-	       ps_set_outputtype(PIG);
-	       if (sscanf(sp,"pig %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.d", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.d");
-	       }
-	   } else if (strncmp(sp,"hpgl",4)==0) {
-	       ps_set_outputtype(HPGL);
-	       if (sscanf(sp,"hpgl %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.hpgl", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.hpgl");
-	       }
-	   } else if (strncmp(sp,"post",4)==0) {
-	       ps_set_outputtype(POSTSCRIPT);
-	       if (sscanf(sp,"post %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.ps", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.ps");
-	       }
-	   } else if (strncmp(sp,"svg",3)==0) {
-	       ps_set_outputtype(SVG);
-	       if (sscanf(sp,"svg %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.svg", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.svg");
-	       }
-	   } else if (strncmp(sp,"dxf",3)==0) {
-	       ps_set_outputtype(DXF);
-	       if (sscanf(sp,"dxf %s", scratch) == 1) {
-		   sprintf(scratch2, "cat > %s.dxf", scratch);
-		   xwin_dump_postscript(scratch2);
-	       } else {
-		   xwin_dump_postscript("cat > pddump.dxf");
-	       }
-	   } else if (strncmp(sp,"exit",4)==0) {
-	       exit(0);
-	   } else {
-	       savecmd(sp);
+       if ((n=sscanf(sp,"%lf %lf",&x,&y))==2) {
+	  savepoint(x,y);
+       } else if (*sp == '#') {		// comment line
+	    ;
+       } else if (strncmp(sp,"nextygraph",10)==0) {
+	   nextygraph();
+       } else if (strncmp(sp,"replot",6)==0) {
+	   replot();
+       } else if (strncmp(sp,"noframe",7)==0) {
+	   gridstate(0);
+	   box(0);
+	   scale(0);
+	   tick(0);
+       } else if (strncmp(sp,"framepen",8)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      frame_pen(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad framepen value: %s\n", s);
 	   }
-       } else if (*sp == '#') {		// else, might be a comment line
-       	   ; 
-       } else {				// nothing left but data point
-           n=sscanf(sp,"%lf %lf",&x,&y);
-	   if (n==2) {
-	      savepoint(x,y);
-	   } else {
-	      if (*sp != '\0' && *sp != '\n') {
-		  fprintf(stderr,"%s: syntax error on line %d: \"%s\"\n", progname, line,s);
-	      }
+       } else if (strncmp(sp,"gridpen",7)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      gridpen(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad grid value: %s\n", s);
 	   }
+       } else if (strncmp(sp,"xgridpen",8)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      xgridpen(itmp); 
+	   } else {
+	      fprintf(stderr, "bad xgrid value: %s\n", s);
+	   }
+       } else if (strncmp(sp,"ygridpen",8)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      ygridpen(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad ygrid value: %s\n", s);
+	   }
+       } else if (strncmp(sp,"xgrid ",6)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      xgrid(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad xgrid value: %s\n", s);
+	   }
+       } else if (strncmp(sp,"ygrid ",6)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      ygrid(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad ygrid value: %s\n", s);
+	   }
+       } else if (strncmp(sp,"grid ",5)==0) {
+	   if (sscanf(s, "%*s %d", &itmp)==1) { 
+	      grid(itmp); 
+	   } else { 
+	      fprintf(stderr, "bad grid value: %s\n", s);
+	   }
+       } else if (strncmp(sp,"brightgrid",10)==0) {
+	   gridpen(1);
+       } else if (strncmp(sp,"dimgrid",7)==0) {
+	   gridpen(12);
+       } else if (strncmp(sp,"linxy",4)==0) {
+	   logmode(0,0);
+	   logmode(0,0);
+	   logmode(1,0);
+       } else if (strncmp(sp,"linx",4)==0) {
+	   logmode(0,0);
+       } else if (strncmp(sp,"liny",4)==0) {
+	   logmode(1,0);
+       } else if (strncmp(sp,"logxy",5)==0) {
+	   logmode(0,1);
+	   logmode(1,1);
+       } else if (strncmp(sp,"logx",4)==0) {
+	   logmode(0,1);
+       } else if (strncmp(sp,"logy",4)==0) {
+	   logmode(1,1);
+       } else if (strncmp(sp,"loglog",6)==0) {
+	   logmode(0,1);
+	   logmode(1,1);
+       } else if (strncmp(sp,"dbxy",4)==0) {
+	   logmode(0,2);
+	   logmode(1,2);
+       } else if (strncmp(sp,"dbx",3)==0) {
+	   logmode(0,2);
+       } else if (strncmp(sp,"dby",3)==0) {
+	   logmode(1,2);
+       } else if (strncmp(sp,"dbpxy",5)==0) {
+	   logmode(0,3);
+	   logmode(1,3);
+       } else if (strncmp(sp,"dbpx",4)==0) {
+	   logmode(0,3);
+       } else if (strncmp(sp,"dbpy",4)==0) {
+	   logmode(1,3);
+       } else if (strncmp(sp,"dump",4)==0) {
+	   dumppoints();
+       } else if (strncmp(sp,"grid",4)==0) {
+	   gridstate(1);
+       } else if (strncmp(sp,"nogrid",6)==0) {
+	   gridstate(0);
+       } else if (strncmp(sp,"box",3)==0) {
+	   box(1);
+       } else if (strncmp(sp,"nobox",5)==0) {
+	   box(0);
+       } else if (strncmp(sp,"noisotropic",11)==0) {
+	   iso(0,1.0);
+       } else if (strncmp(sp,"isotropic",9)==0) {
+	   if (sscanf(sp+9,"%lg", &tmp) != 1) {
+	       iso(1,1.0);
+	   } else {
+	       iso(1,tmp);
+	   }
+       } else if (strncmp(sp,"plot",4)==0) {
+	   need_redraw++;
+	   xwin_top();
+       } else if (strncmp(sp,"clear",5)==0) {
+	   initplot();	
+       } else if (strncmp(sp,"xscaletol",9)==0) {
+	   if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
+	      fprintf(stderr, "bad xscaletol values: %s\n",sp);
+	   } else {
+	      xscaletol(tmp);	
+	   }
+       } else if (strncmp(sp,"yscaletol",9)==0) {
+	   if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
+	      fprintf(stderr, "bad yscaletol values: %s\n",sp);
+	   } else {
+	      yscaletol(tmp);	
+	   }
+       } else if (strncmp(sp,"scaletol",8)==0) {
+	   if (sscanf(sp,"%*s %lg", &tmp) != 1 || tmp >= 0.4 || tmp < -1.0) {
+	      fprintf(stderr, "bad scaletol values: %s\n",sp);
+	   } else {
+	      scaletol(tmp);	
+	   }
+       } else if (strncmp(sp,"ticklength",10)==0) {
+	   if (sscanf(sp+10,"%lg", &tmp) != 1 || tmp < 0.1 || tmp > 10.0) {
+	      tickset(-1.0);
+	   } else {
+	       tickset(tmp);	
+	   }
+       } else if (strncmp(sp,"xset",4)==0) {
+	   if (sscanf(sp+4,"%lg %lg", &xmin, &xmax ) != 2 || xmin > xmax) {
+	      fprintf(stderr, "bad xset values: %s\n",sp);
+	   } else {
+	       // fprintf(stderr, "xset values: %g %g\n", xmin, xmax);
+	       xset(xmin, xmax);	
+	   }
+       } else if (strncmp(sp,"yset",4)==0) {
+	   if (sscanf(sp+4,"%lg %lg", &ymin, &ymax ) != 2 || ymin > ymax) {
+	      fprintf(stderr, "bad yset values: %s\n",sp);
+	   } else {
+	       // fprintf(stderr, "yset values: %g %g\n", ymin, ymax);
+	       yset(ymin, ymax);	
+	   }
+       } else if (strncmp(sp,"xscale",6)==0) {
+	   if (sscanf(sp,"xscale %lg %[^#]", &tmp, scratch ) == 2) {
+	      xscale(scratch,tmp);	
+	   } else if (sscanf(sp,"xscale %[^#]", scratch ) == 1) {
+	      xscale(scratch,1.0);	
+	   } else {
+	      fprintf(stderr, "bad xscale values: %s\n", sp);
+	   }
+       } else if (strncmp(sp,"yscale",6)==0) {
+	   if (sscanf(sp,"yscale %lg %[^#]", &tmp, scratch ) == 2) {
+	      yscale(scratch,tmp);	
+	   } else if (sscanf(sp,"yscale %[^#]", scratch ) == 1) {
+	      yscale(scratch,1.0);	
+	   } else {
+	      fprintf(stderr, "bad yscale values: %s\n", sp);
+	   }
+       } else if (strncmp(sp,"titlesize",9)==0) {	
+	   savecmd(sp); 	// prevent "title" from eating titlesize
+       } else if (strncmp(sp,"title",5)==0) {
+	   title(sp+6);
+       } else if (strncmp(sp,"style",5)==0) {
+	   sp=skipblanks(sp+6);
+	   if (sp[0] == 'p') {
+	      gridstate(0); 
+	      setcharsize(1.6);
+	   } else if (sp[0] == 'w') {
+	      gridstate(1);
+	      setcharsize(1.0);
+	   }
+       } else if (strncmp(sp,"graph",5)==0) {
+	   if (sscanf(sp,"graph %s", scratch) == 1) {
+	       sprintf(scratch2, "pnmtopng > %s.png", scratch);
+	       xwin_dump_graphics(scratch2);
+	   } else {
+	       xwin_dump_graphics("pnmtopng > pddump.png");
+	   }
+       } else if (strncmp(sp,"autoplot",8)==0) {
+	   ps_set_outputtype(AUTOPLOT);
+	   if (sscanf(sp,"autoplot %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.ap", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.ap");
+	   }
+       } else if (strncmp(sp,"pig",3)==0) {
+	   ps_set_outputtype(PIG);
+	   if (sscanf(sp,"pig %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.d", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.d");
+	   }
+       } else if (strncmp(sp,"hpgl",4)==0) {
+	   ps_set_outputtype(HPGL);
+	   if (sscanf(sp,"hpgl %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.hpgl", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.hpgl");
+	   }
+       } else if (strncmp(sp,"post",4)==0) {
+	   ps_set_outputtype(POSTSCRIPT);
+	   if (sscanf(sp,"post %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.ps", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.ps");
+	   }
+       } else if (strncmp(sp,"svg",3)==0) {
+	   ps_set_outputtype(SVG);
+	   if (sscanf(sp,"svg %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.svg", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.svg");
+	   }
+       } else if (strncmp(sp,"dxf",3)==0) {
+	   ps_set_outputtype(DXF);
+	   if (sscanf(sp,"dxf %s", scratch) == 1) {
+	       sprintf(scratch2, "cat > %s.dxf", scratch);
+	       xwin_dump_postscript(scratch2);
+	   } else {
+	       xwin_dump_postscript("cat > pddump.dxf");
+	   }
+       } else if (strncmp(sp,"exit",4)==0) {
+	   exit(0);
+       } else if (strncmp(sp,"noback",6)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"back",4)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"jump",4)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"label",5)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"symbol",6)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"tagsize",7)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"scalesize",9)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"symbolsize",10)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"autopen",7)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"noautosymbol",12)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"pen",3)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"noautopen",9)==0) {
+	   savecmd(sp);
+       } else if (strncmp(sp,"line",4)==0) {
+	   savecmd(sp);
+       } else {
+	  if (*sp != '\0' && *sp != '\n') {
+	      fprintf(stderr,"%s: syntax error on line %d: \"%s\"\n", progname, line,s);
+	  }
        }
     }
     usleep(500); 	// don't hog CPU
---
+++ tests/color.awk
@@ -0,0 +1,26 @@
+(
+   echo 0 0
+   echo 1 1
+   echo pen 1
+   echo label 0.1 0.1 pen1
+   echo pen 2
+   echo label 0.2 0.2 pen2
+   echo pen 3
+   echo label 0.3 0.3 pen3
+   echo pen 4
+   echo label 0.4 0.4 pen4
+   echo pen 5
+   echo label 0.5 0.5 pen5
+   echo pen 6
+   echo label 0.6 0.6 pen6
+   echo pen 7
+   echo label 0.7 0.7 pen7
+   echo pen 8
+   echo label 0.8 0.8 pen8
+   echo pen 9
+   echo label 0.9 0.9 pen9
+   echo pen 10
+   echo label 1.0 1.0 pen10
+   echo post
+   echo dxf
+)
